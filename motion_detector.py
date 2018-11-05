3f822b0dbccd2991aee7db9609691e3bfc33fbb4
Arati Kulkarni <aratik711@gmail.com>
2018-11-05 20:07:36 +0530

    commit

--- motion_detector.py
+++ motion_detector.py
@@ -1,14 +1,41 @@
-import cv2, time
+import cv2
+
+first_frame = None
 
 video=cv2.VideoCapture(0)
+a = 0
+
 while True:
     check, frame = video.read()
 
-    print(check)
-    print(frame)
-
-    cv2.imshow("capturing", frame)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    gray = cv2.GaussianBlur(gray, (21, 21), 0)
+
+
+
+    if (a < 40):
+        a += 1
+        first_frame = gray
+        continue
+
+    delta_frame = cv2.absdiff(first_frame, gray)
+
+    thresh_delta = cv2.threshold(delta_frame, 30, 255, cv2.THRESH_BINARY)[1]
+
+    thresh_frame = cv2.dilate(thresh_delta, None, iterations=2)
+
+    (_,cnts,_)=cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+        (x, y, w, h) = cv2.boundingRect(contour)
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+
+    cv2.imshow("Gray", gray)
+    cv2.imshow("Delta", delta_frame)
+    cv2.imshow("Threshold", thresh_delta)
+    cv2.imshow("rectangle", frame)
 
     key=cv2.waitKey(1)
 
