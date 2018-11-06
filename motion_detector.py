d482562331d54f943fa2e4d74d74040a2e4eb70f
Arati Kulkarni <aratik711@gmail.com>
2018-11-06 11:53:25 +0530

    added time data

--- motion_detector.py
+++ motion_detector.py
@@ -1,9 +1,14 @@
-import cv2
+import cv2, pandas
+from datetime import datetime
 
 first_frame = None
 
 video=cv2.VideoCapture(0)
 a = 0
+status_list = [None, None]
+times = []
+
+df = pandas.DataFrame(columns=["Start", "End"])
 
 while True:
     check, frame = video.read()
@@ -11,7 +16,7 @@ while True:
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-
+    status = 0
 
     if (a < 40):
         a += 1
@@ -27,10 +32,18 @@ while True:
     (_,cnts,_)=cv2.findContours(thresh_frame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
     for contour in cnts:
-        if cv2.contourArea(contour) < 1000:
+        if cv2.contourArea(contour) < 10000:
             continue
+        status = 1
         (x, y, w, h) = cv2.boundingRect(contour)
         cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3)
+
+    status_list.append(status)
+
+    if (status_list[-1]==1) and (status_list[-2] == 0):
+        times.append(datetime.now())
+    if (status_list[-1]==0) and (status_list[-2] == 1):
+        times.append(datetime.now())
 
     cv2.imshow("Gray", gray)
     cv2.imshow("Delta", delta_frame)
@@ -40,6 +53,17 @@ while True:
     key=cv2.waitKey(1)
 
     if key == ord('q'):
+        if status==1:
+            times.append(datetime.now())
         break
+
+print(status_list)
+print(times)
+
+for i in range(0,len(times),2):
+    df = df.append({"Start": times[i],"End": times[i+1]},ignore_index=True)
+
+df.to_csv("times.csv")
+
 video.release()
 cv2.destroyAllWindows()
